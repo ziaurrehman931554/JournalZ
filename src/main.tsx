a2eec340b06d054b6b1975baf75b5157382350f3
Zia Ur Rehman <ziaurrehman931554@users.noreply.github.com>
2026-06-08 16:54:24 +0500

    Fix BrowserRouter basename for GitHub Pages sub-path deployment

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,11 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
 import "./index.css";
 
+const basename = import.meta.env.BASE_URL.replace(/\/+$/, "");
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <ThemeProvider>
         <App />
       </ThemeProvider>
